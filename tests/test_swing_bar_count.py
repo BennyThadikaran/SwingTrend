08bb02bff3a1bf038ec00170abaf85699ce93203
Benny Thadikaran <tbenny05@yahoo.com>
2025-03-08 23:55:09 +0530

    Fix tests for incorrect dict key name for bars_since

--- tests/test_swing_bar_count.py
+++ tests/test_swing_bar_count.py
@@ -50,7 +50,9 @@ class TestSwingBarCount(unittest.TestCase):
 
     def test_uptrend_new_high_with_close_below_sph(self):
         """New Bar high above SPH but close below SPH. Bar count is reset to 0"""
-        self.swing.unpack(dict(trend="UP", sph=100, high=100, bars_since=5))
+        self.swing.unpack(
+            dict(trend="UP", sph=100, high=100, _Swing__bars_since=5)
+        )
 
         self.swing.identify(datetime(2024, 1, 1), high=105, low=95, close=99)
 
@@ -58,7 +60,9 @@ class TestSwingBarCount(unittest.TestCase):
 
     def test_downtrend_new_low_with_close_above_spl(self):
         """New Bar low below SPL but close above SPL. Bar count is reset to 0"""
-        self.swing.unpack(dict(trend="DOWN", spl=100, low=100, bars_since=5))
+        self.swing.unpack(
+            dict(trend="DOWN", spl=100, low=100, _Swing__bars_since=5)
+        )
 
         self.swing.identify(datetime(2024, 1, 1), high=105, low=95, close=102)
 
@@ -70,7 +74,7 @@ class TestSwingBarCount(unittest.TestCase):
             dict(
                 trend="UP",
                 sph=100,
-                bars_since=5,
+                _Swing__bars_since=5,
                 low_dt=datetime(2023, 12, 25),
             )
         )
@@ -85,7 +89,7 @@ class TestSwingBarCount(unittest.TestCase):
             dict(
                 trend="DOWN",
                 spl=100,
-                bars_since=5,
+                _Swing__bars_since=5,
                 low_dt=datetime(2023, 12, 25),
             )
         )
@@ -96,7 +100,7 @@ class TestSwingBarCount(unittest.TestCase):
 
     def test_uptrend_new_high_with_no_sph(self):
         """No SPH formed but new high. Bar count is reset to 0"""
-        self.swing.unpack(dict(trend="UP", high=100, bars_since=5))
+        self.swing.unpack(dict(trend="UP", high=100, _Swing__bars_since=5))
 
         self.swing.identify(datetime(2024, 1, 1), high=105, low=95, close=103)
 
@@ -104,7 +108,7 @@ class TestSwingBarCount(unittest.TestCase):
 
     def test_downtrend_new_low_with_no_spl(self):
         """No SPL formed but new low. Bar count is reset to 0"""
-        self.swing.unpack(dict(trend="DOWN", low=100, bars_since=5))
+        self.swing.unpack(dict(trend="DOWN", low=100, _Swing__bars_since=5))
 
         self.swing.identify(datetime(2024, 1, 1), high=105, low=95, close=103)
 
@@ -112,7 +116,9 @@ class TestSwingBarCount(unittest.TestCase):
 
     def test_uptrend_continuation(self):
         """SPH is set and new SPH formed. Bar count is reset to 0"""
-        self.swing.unpack(dict(trend="UP", sph=100, high=105, bars_since=5))
+        self.swing.unpack(
+            dict(trend="UP", sph=100, high=105, _Swing__bars_since=5)
+        )
 
         self.swing.identify(datetime(2024, 1, 1), high=103, low=95, close=102)
 
@@ -120,7 +126,9 @@ class TestSwingBarCount(unittest.TestCase):
 
     def test_downtrend_continuation(self):
         """SPL is set and new SPL formed. Bar count is reset to 0"""
-        self.swing.unpack(dict(trend="DOWN", spl=100, low=95, bars_since=5))
+        self.swing.unpack(
+            dict(trend="DOWN", spl=100, low=95, _Swing__bars_since=5)
+        )
 
         self.swing.identify(datetime(2024, 1, 1), high=103, low=93, close=93)
 
@@ -133,7 +141,7 @@ class TestSwingBarCount(unittest.TestCase):
                 trend="UP",
                 sph=100,
                 coc=95,
-                bars_since=5,
+                _Swing__bars_since=5,
                 high_dt=datetime(2023, 12, 26),
             )
         )
@@ -150,7 +158,7 @@ class TestSwingBarCount(unittest.TestCase):
                 trend="DOWN",
                 spl=95,
                 coc=100,
-                bars_since=5,
+                _Swing__bars_since=5,
                 low_dt=datetime(2023, 12, 26),
             )
         )
@@ -200,7 +208,7 @@ class TestSwingBarCount(unittest.TestCase):
             dict(
                 trend="UP",
                 high=105,
-                bars_since=0,
+                _Swing__bars_since=0,
                 high_dt=datetime(2024, 12, 25),
             )
         )
@@ -216,7 +224,7 @@ class TestSwingBarCount(unittest.TestCase):
             dict(
                 trend="DOWN",
                 low=100,
-                bars_since=0,
+                _Swing__bars_since=0,
                 low_dt=datetime(2024, 12, 25),
             )
         )
